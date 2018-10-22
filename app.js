b032ccf6827fba48ec10e8b1480b7b67157ea9e6
ryam <ryambudha@gmail.com>
2018-10-22 18:26:15 +0530

    initializaion

---
+++ app.js
@@ -0,0 +1,144 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cron = require('node-cron');
+var fs = require('fs');
+var cheerio =  require('cheerio');
+var URL = require('url-parse');
+var request = require('request');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+cron.schedule("* * * * *", function(){
+  console.log('Runs every minutes');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+var startURL = "http://www.arstechnica.com";
+var search_word = 'stemming';
+var max_page_to_visit = 10;
+
+var page_visited= {};
+var no_of_page_visited = 0;
+var page_to_visit = [];
+var url = new URL(startURL);
+var baseUrl = url.protocol + '//' + url.hostname;
+page_to_visit.push(startURL);
+crawl();
+
+function crawl(){
+  if (no_of_page_visited >= max_page_to_visit) {
+    console.log("Maximum Pages Reached");
+    return;
+  }
+  var nextPage = page_to_visit.pop();
+
+  if (nextPage in page_visited) {
+    crawl();
+  }else{
+    //new page we haven't visited
+    visitPage(nextPage, crawl);
+  }
+}
+function visitPage(nextPage, callback){
+      //add page to our set
+      page_visited[url] = true;
+      no_of_page_visited++;
+
+      //make the request 
+      request(nextPage, function(err, response, body){
+        if (response.statusCode !== 200) {
+          callback();
+          return;  
+        }
+      })
+}
+
+request(pageToVisit, function(err, response, body){
+  if (err) {
+    console.log(err);
+  }
+  if (response.statusCode === 200) {
+    var $ = cheerio.load(body);
+    console.log("Page Title", $('title').text());
+  }
+});
+
+function searchForWords($, word){
+  var bodyText = $('html > body').text();
+  if (bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+    return true;
+  }
+  return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
