55b6fbd4e85d920cda9e06a9566b0d94c0682ff1
ryam <ryambudha@gmail.com>
2018-10-26 17:41:14 +0530

    heroku final commit

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-cron.schedule("41 * * * *", function(){
+cron.schedule("42 * * * *", function(){
   console.log("cron intitialize");
   index.crawlSeasons(SEASON_URI);
   index.crawlMovies(MOVIE_URI);
