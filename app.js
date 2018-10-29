4f4da559105c6815d39770fc2536b5934a7667e0
ryam <ryambudha@gmail.com>
2018-10-29 10:59:14 +0530

    at every 15 minutes of an hour

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(function (req, res, next) {
 // });
 if (isProduction) {
  var crawlerCronJob = new CronJob({
-    cronTime: '0 55 * * * *',
+    cronTime: '0 0 * * * *',
     onTick: function () {
       index.crawlMovies(MOVIE_URI);
       index.crawlSeasons(SEASON_URI);
